4683bd13ce4dbaf76eab3c6422626d526bfdcbc8
cuscamayta <uscamayta.carlos@gmail.com>
2016-10-21 18:11:54 -0400

    modelo itinerario y programacion diaria

--- server/models/office.js
+++ server/models/office.js
@@ -9,6 +9,7 @@ module.exports = function (sequelize, DataTypes) {
       classMethods: {
         associate: function (models) {
           Office.belongsTo(models.Destination, { foreignKey: "idorigin" });
+          Office.hasMany(models.Travel, { foreignKey: 'idoffice' });
         }
       }
     }
