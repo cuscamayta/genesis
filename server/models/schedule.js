d3bf779e8c948251e734ae797864203c06ccdca5
cuscamayta <uscamayta.carlos@gmail.com>
2016-10-26 09:18:04 -0400

    precio

--- server/models/schedule.js
+++ server/models/schedule.js
@@ -3,6 +3,7 @@
 module.exports = function (sequelize, DataTypes) {
     var Schedule = sequelize.define("Schedule", {
         dateregister: { type: DataTypes.DATE, allowNull: false },
+        price:  { type: DataTypes.DECIMAL, allowNull: false },
         arrival: { type: DataTypes.STRING, allowNull: false },
         departure: { type: DataTypes.STRING, allowNull: false },
         detail: { type: DataTypes.STRING, allowNull: true }
